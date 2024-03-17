c93394a7d0357eac0315646d671df0e1f3f76c46
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-03-17 22:49:44 +0500

    String and If else

--- Revisiting-Python/p07.py
+++ Revisiting-Python/p07.py
@@ -9,3 +9,18 @@ print (len(x))
 
 print ("Dany" in x)
 print ("Dany" not in x)
+
+print (x[:4])
+print (x[-3:])
+
+print (x.upper())
+
+y = "  Dani  "
+print (y)
+print (y.strip())
+
+print(x.split("y"))
+
+age = 18
+txt = "My age is: {}"
+print (txt.format(age))
