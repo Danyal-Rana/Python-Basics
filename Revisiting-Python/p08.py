b403540d00feb2783e7b9a79f4910f21a5da63f6
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-03-17 22:54:58 +0500

    if-elif

--- Revisiting-Python/p08.py
+++ Revisiting-Python/p08.py
@@ -1,3 +1,5 @@
+# if-elif ladder
+
 a = int (input("Enter first number: "))
 b = int (input("Enter second number: "))
 
@@ -7,3 +9,8 @@ elif (b>a):
     print ("b is greater than a")
 else:
     print ("Both a and b are equal")
+
+print ("a is greater than b") if a>b else print("b is greater than a") if b>a else print("Both are equal")
+
+if a>b or b>a:
+    pass
